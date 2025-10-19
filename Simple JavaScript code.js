ee5339462689416c1d97d1b2444a6fab103a09d9
Rajendra Kurmi <rajendrakurmi78@gmail.com>
2025-10-19 19:27:48 +0530

    Create Simple JavaScript code.js

---
+++ Simple JavaScript code.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello World Example</title>
+</head>
+<body>
+  <script>
+    document.write("Hello World");  
+    console.log("Hello World");    
+  </script>
+</body>
+</html>
